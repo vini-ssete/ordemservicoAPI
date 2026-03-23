892a94c831ef824899af523c2b36c4a8a11989d4
vinicius <viniciuscdp1609@gmail.com>
2026-03-23 16:31:49 -0300

    Commit V 1.0 - Início do projeto de Ordem de Serviço

---
+++ src/test/java/br/eti/vinicius/osAPI/OsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eti.vinicius.osAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
